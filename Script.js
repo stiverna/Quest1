652d11e827daa83cd63b50fbd20fefbf4b5f75aa
stiverna <diyorbekbegimqulov10@gmail.com>
2025-07-23 11:09:49 +0500

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,7 @@
+document.getElementById("registerBtn").addEventListener("click", function () {
+  alert("Ruyhatdan o‘tish oynasi hozircha tayyor emas 😊");
+});
+
+document.getElementById("loginBtn").addEventListener("click", function () {
+  alert("Tizimga kirish oynasi hozircha tayyor emas 😊");
+});
